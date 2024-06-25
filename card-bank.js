9b68bd80e76265355bfc2a0a920430e4ff3023ab
Anton <italisman04@gmail.com>
2024-06-25 14:56:55 +0300

    Second Version0

--- card-bank.js
+++ card-bank.js
@@ -2,26 +2,71 @@ var visaIcon = "img/visa.png";
 var masterIcon = "img/mastercard.png";
 var pay = document.getElementById('variety-list');
 var cardSelect = /** @class */ (function () {
-    function cardSelect(cardNumber, expiration, logo) {
+    function cardSelect(cardNumber, expiration, name, cvvNumber) {
         this.cardNumber = cardNumber;
         this.expiration = expiration;
-        var splitCardNumber = cardNumber.toString().split('');
-        if (splitCardNumber[0] === '4') {
-            this.logo = visaIcon;
-        }
-        else if (splitCardNumber[0] === '5') {
-            this.logo = masterIcon;
-        }
+        this.name = name;
+        this.cvvNumber = cvvNumber;
+        this.logo = this.splitCardNumber(cardNumber);
+        this.expInfo = this.checkExpiration(expiration) ? "Expired" : "Expires";
     }
+    cardSelect.prototype.splitCardNumber = function (cardNumber) {
+        var firstDigit = cardNumber.toString()[0];
+        if (firstDigit === '4') {
+            return visaIcon;
+        }
+        else if (firstDigit === '5') {
+            return masterIcon;
+        }
+        else {
+            return 'alert("Wrong card!")';
+        }
+    };
+    cardSelect.prototype.checkExpiration = function (expiration) {
+        var _a = expiration.split('/').map(Number), expMonth = _a[0], expYear = _a[1];
+        var expDate = new Date(expYear + 2000, expMonth - 1, 1);
+        var currentDate = new Date();
+        return expDate < currentDate;
+    };
     return cardSelect;
 }());
-var card = new cardSelect(434234234, "09/27");
-var cardLogo = document.createElement("img");
-var cardNum = document.createElement("p");
-var cardDate = document.createElement("p");
-cardLogo.src = card.logo;
-pay.appendChild(cardLogo);
-cardNum.innerHTML = card.cardNumber.toString();
-pay.appendChild(cardNum);
-cardDate.innerHTML = "Expiries " + card.expiration;
-pay.appendChild(cardDate);
+var cards = [];
+var addButton = document.getElementById('buttSubmit');
+addButton === null || addButton === void 0 ? void 0 : addButton.addEventListener('click', addCard);
+var savedCards = localStorage.getItem('cards');
+if (savedCards) {
+    cards = JSON.parse(savedCards);
+}
+cards.forEach(function (card) {
+    displayCard(card);
+});
+function addCard() {
+    var name = document.getElementById('nameField').value;
+    var cardNumber = parseInt(document.getElementById('numberField').value);
+    var expiration = document.getElementById('expField').value;
+    var cvvNumber = document.getElementById('cvvField').value;
+    if (isNaN(cardNumber) || !expiration || !cvvNumber) {
+        alert("Please fill in all fields correctly.");
+        return;
+    }
+    var newCard = new cardSelect(cardNumber, expiration, name, cvvNumber);
+    cards.push(newCard);
+    displayCard(newCard);
+    localStorage.setItem("cards", JSON.stringify(cards));
+}
+function displayCard(card) {
+    var cardLogo = document.createElement("img");
+    var cardNum = document.createElement("p");
+    var cardStatus = document.createElement("p");
+    var cardDate = document.createElement("p");
+    var li = document.createElement("li");
+    cardLogo.src = card.logo;
+    li.appendChild(cardLogo);
+    cardNum.innerHTML = card.cardNumber.toString();
+    li.appendChild(cardNum);
+    cardStatus.innerHTML = card.expInfo;
+    li.appendChild(cardStatus);
+    cardDate.innerHTML = card.expiration;
+    li.appendChild(cardDate);
+    pay.appendChild(li);
+}
--- card-bank.ts
+++ card-bank.ts
@@ -1,33 +1,96 @@
 const visaIcon: string = "img/visa.png";
-const masterIcon: string = "img/mastercard.png"
+const masterIcon: string = "img/mastercard.png";
 const pay = document.getElementById('variety-list') as HTMLElement;
 
+class cardSelect {
+    cardNumber: number;
+    expiration: string;
+    logo: string;
+    expInfo: string;
+    name: string;
+    cvvNumber: string;
 
-class cardSelect{
-    cardNumber: number
-    expiration: string
-    logo!: string;
-    constructor(cardNumber: number, expiration: string, logo?: string) {
+    constructor(cardNumber: number, expiration: string, name: string, cvvNumber: string) {
         this.cardNumber = cardNumber;
         this.expiration = expiration;
-        const splitCardNumber = cardNumber.toString().split('');
-        if (splitCardNumber[0] === '4') {
-            this.logo = visaIcon;
-        } else if (splitCardNumber[0] === '5') {
-            this.logo = masterIcon;
-        } 
+        this.name = name;
+        this.cvvNumber = cvvNumber;
+        this.logo = this.splitCardNumber(cardNumber);
+        this.expInfo = this.checkExpiration(expiration) ? "Expired" : "Expires";
     }
+    
+    splitCardNumber(cardNumber: number): string {
+        const firstDigit = cardNumber.toString()[0];
+        if (firstDigit  === '4') {
+            return visaIcon;
+        } else if (firstDigit  === '5') {
+            return masterIcon;
+        } else {
+            return 'alert("Wrong card!")';
+        }
+    }
+
+    checkExpiration(expiration: string): boolean {
+        const [expMonth, expYear] = expiration.split('/').map(Number);
+        const expDate = new Date(expYear + 2000, expMonth - 1, 1);
+        const currentDate = new Date();
+        return expDate < currentDate;
+    }
+
+    
 }
 
-const card = new cardSelect(434234234, "09/27");
+type TypeArray = cardSelect[];
 
-const cardLogo = document.createElement("img");
-const cardNum = document.createElement("p");
-const cardDate = document.createElement("p");
+let cards: TypeArray = [];
 
-cardLogo.src = card.logo;
-pay.appendChild(cardLogo);
-cardNum.innerHTML = card.cardNumber.toString();
-pay.appendChild(cardNum);
-cardDate.innerHTML = "Expiries " + card.expiration;
-pay.appendChild(cardDate);
+const addButton = document.getElementById('buttSubmit');
+addButton?.addEventListener('click', addCard);
+
+const savedCards = localStorage.getItem('cards');
+if (savedCards) {
+    cards = JSON.parse(savedCards);
+}
+
+cards.forEach(function(card){
+    displayCard(card)
+})
+
+
+function addCard() {
+    const name = (document.getElementById('nameField') as HTMLInputElement).value;
+    const cardNumber = parseInt((document.getElementById('numberField') as HTMLInputElement).value);
+    const expiration = (document.getElementById('expField') as HTMLInputElement).value;
+    const cvvNumber = (document.getElementById('cvvField') as HTMLInputElement).value;
+
+    if (isNaN(cardNumber) || !expiration || !cvvNumber) {
+        alert("Please fill in all fields correctly.");
+        return;
+    }
+
+    const newCard = new cardSelect(cardNumber, expiration, name, cvvNumber);
+    cards.push(newCard);
+    displayCard(newCard);
+
+    localStorage.setItem("cards", JSON.stringify(cards));
+}
+
+function displayCard(card: cardSelect) {
+    const cardLogo = document.createElement("img");
+    const cardNum = document.createElement("p");
+    const cardStatus = document.createElement("p");
+    const cardDate = document.createElement("p");
+    const li = document.createElement("li");
+
+    cardLogo.src = card.logo;
+    li.appendChild(cardLogo);
+    cardNum.innerHTML = card.cardNumber.toString();
+    li.appendChild(cardNum);
+    cardStatus.innerHTML = card.expInfo;
+    li.appendChild(cardStatus);
+    cardDate.innerHTML = card.expiration;
+    li.appendChild(cardDate);
+
+    pay.appendChild(li);
+}
+
